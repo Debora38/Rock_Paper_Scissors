15f26c380014c9fa0073128f0bac43417223adaf
Debora <debora.piu92@gmail.com>
2019-01-03 21:06:28 +0000

    encapsulating blocks into methods for a cleaner code

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -1,19 +1,49 @@
-puts "Let's play Rock, Paper, Scissors! \nGet ready with you choice!"
-user_choice = gets.chomp.capitalize
-while user_choice != "Stop"
-computer_choice = ["Rock", "Paper", "Scissors"].sample
-  if user_choice != "Rock" && user_choice != "Paper" && user_choice != "Scissors"
-    puts "I did not understand your choice. Try again or type \"stop\" to end the game."
-	  user_choice = gets.chomp.capitalize
-  end
-  if user_choice == computer_choice
-	   puts "We both played #{user_choice}! \nLet's play again or type \"stop\" to end the game."
-    user_choice = gets.chomp.capitalize
-  elsif user_choice == "Rock" && computer_choice == "Paper" || user_choice == "Paper" && computer_choice == "Scissors" || user_choice == "Scissors" && computer_choice == "Rock"
-	    puts "I played #{computer_choice}! I WIN! \n(Let's play again or type \"stop\" to end the game."
-      user_choice = gets.chomp.capitalize
-  elsif user_choice == "Paper" && computer_choice == "Rock" || user_choice == "Scissors" && computer_choice == "Paper" || user_choice == "Rock" && computer_choice == "Scissors"
-	     puts "I played #{computer_choice}! YOU WIN! \nLet's play again or type \"stop\" to end the game."
-       user_choice = gets.chomp.capitalize
-  end
+def tie(choice)
+  puts "We both played #{choice}! \nLet's play again or type \"stop\" to end the game."
 end
+
+def computer_wins?(user_choice, computer_choice)
+  user_choice == "Paper" && computer_choice == "Rock" ||
+  user_choice == "Scissors" && computer_choice == "Paper" ||
+  user_choice == "Rock" && computer_choice == "Scissors"
+end
+
+def computer_won(computer_choice)
+  puts "I played #{computer_choice}! YOU WIN! \nLet's play again or type \"stop\" to end the game."
+end
+
+def user_wins?(user_choice, computer_choice)
+  user_choice == "Rock" && computer_choice == "Paper" ||
+  user_choice == "Paper" && computer_choice == "Scissors" ||
+  user_choice == "Scissors" && computer_choice == "Rock"
+end
+
+def user_won(user_choice, computer_choice)
+  puts "I played #{computer_choice}! I WIN! \n(Let's play again or type \"stop\" to end the game."
+end
+
+def wrong_entry(user_choice)
+  user_choice != "Rock" &&
+  user_choice != "Paper" &&
+  user_choice != "Scissors"
+end
+
+def run_game
+  puts "Let's play Rock, Paper, Scissors! \nGet ready with you choice!"
+  user_choice = gets.chomp.capitalize
+    while user_choice != "Stop"
+    computer_choice = ["Rock", "Paper", "Scissors"].sample
+      if wrong_entry(user_choice)
+        puts "I did not understand your choice. Try again or type \"stop\" to end the game."
+      elsif user_choice == computer_choice
+	      tie(user_choice)
+      elsif user_wins?(user_choice, computer_choice)
+        user_won(user_choice, computer_choice)
+      elsif computer_wins?(user_choice, computer_choice)
+        computer_won(computer_choice)
+      end
+      user_choice = gets.chomp.capitalize
+    end
+end
+
+run_game
