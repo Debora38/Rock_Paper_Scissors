e76969528615996081e26e28b5b26d70c110a5b9
Debora <debora.piu92@gmail.com>
2019-01-03 21:46:18 +0000

    more encapsulating and game_over created to show the final score

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -6,7 +6,8 @@ def computer_selects
 end
 
 def tie
-  puts "We both played #{@computer_choice}! \nLet's play again or type \"stop\" to end the game."
+  puts "We both played #{@computer_choice}!"
+  play_again?
 end
 
 def user_wins?(user_choice)
@@ -19,6 +20,7 @@ def user_won
   @user_score += 1
   puts "I played #{@computer_choice}! YOU WIN!"
   puts_score
+  play_again?
 end
 
 def computer_wins?(user_choice)
@@ -31,11 +33,15 @@ def computer_won
   @computer_score += 1
   puts "I played #{@computer_choice}! I WIN!"
   puts_score
+  play_again?
 end
 
 def puts_score
   puts "Computer: #{@computer_score} vs. User: #{@user_score}"
-  puts "(Let's play again or type \"stop\" to end the game."
+end
+
+def play_again?
+  puts "(Let's play again or type \"stop\" to end the game.)"
 end
 
 def wrong_entry(user_choice)
@@ -44,22 +50,35 @@ def wrong_entry(user_choice)
   user_choice != "Scissors"
 end
 
+def game_over
+  puts_score
+  if @computer_score > @user_score
+    puts "It seems like I won!\nGood match and come back again!"
+  elsif @computer_score < @user_score
+    puts "You defeated me!\nWell played and come back again!"
+  else
+    puts "It was a tie!\nWell played and come back again!"
+  end
+  exit
+end
+
 def run_game
-  puts "Let's play Rock, Paper, Scissors! \nGet ready with you choice!"
-  user_choice = gets.chomp.capitalize
-    while user_choice != "Stop"
-      computer_selects
-      if wrong_entry(user_choice)
-        puts "I did not understand your choice. Try again or type \"stop\" to end the game."
-      elsif user_choice == @computer_choice
-	      tie
-      elsif user_wins?(user_choice)
-        user_won
-      elsif computer_wins?(user_choice)
-        computer_won
-      end
-      user_choice = gets.chomp.capitalize
+  puts "Let's play Rock, Paper, Scissors! \nGet ready with your choice!"
+  loop do
+    user_choice = gets.chomp.capitalize
+    computer_selects
+    if user_choice == "Stop"
+      game_over
+    elsif wrong_entry(user_choice)
+      puts "I did not understand your choice. Try again or type \"stop\" to end the game."
+    elsif user_choice == @computer_choice
+      tie
+    elsif user_wins?(user_choice)
+      user_won
+    elsif computer_wins?(user_choice)
+      computer_won
     end
+  end
 end
 
 run_game
