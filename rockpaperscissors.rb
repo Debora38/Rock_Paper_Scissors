4776edd4b2b010a6770a0b1753336084292264bf
Debora <debora.piu92@gmail.com>
2019-01-03 21:14:42 +0000

    computer wins or user wins fixed as contrary was created in previous version

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -2,23 +2,23 @@ def tie(choice)
   puts "We both played #{choice}! \nLet's play again or type \"stop\" to end the game."
 end
 
-def computer_wins?(user_choice, computer_choice)
+def user_wins?(user_choice, computer_choice)
   user_choice == "Paper" && computer_choice == "Rock" ||
   user_choice == "Scissors" && computer_choice == "Paper" ||
   user_choice == "Rock" && computer_choice == "Scissors"
 end
 
-def computer_won(computer_choice)
+def user_won(computer_choice)
   puts "I played #{computer_choice}! YOU WIN! \nLet's play again or type \"stop\" to end the game."
 end
 
-def user_wins?(user_choice, computer_choice)
+def computer_wins?(user_choice, computer_choice)
   user_choice == "Rock" && computer_choice == "Paper" ||
   user_choice == "Paper" && computer_choice == "Scissors" ||
   user_choice == "Scissors" && computer_choice == "Rock"
 end
 
-def user_won(user_choice, computer_choice)
+def computer_won(user_choice, computer_choice)
   puts "I played #{computer_choice}! I WIN! \n(Let's play again or type \"stop\" to end the game."
 end
 
@@ -29,6 +29,7 @@ def wrong_entry(user_choice)
 end
 
 def run_game
+
   puts "Let's play Rock, Paper, Scissors! \nGet ready with you choice!"
   user_choice = gets.chomp.capitalize
     while user_choice != "Stop"
