fcbf7a458e625a6f2a4936b75b63403a071e2b0a
Debora <debora.piu92@gmail.com>
2019-01-03 21:27:46 +0000

    more encapsulating and computer_choice has been made global variable for cleaner code

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -1,31 +1,35 @@
 @computer_score = 0
 @user_score = 0
 
-def tie(choice)
-  puts "We both played #{choice}! \nLet's play again or type \"stop\" to end the game."
+def computer_selects
+  @computer_choice = ["Rock", "Paper", "Scissors"].sample
 end
 
-def user_wins?(user_choice, computer_choice)
-  user_choice == "Paper" && computer_choice == "Rock" ||
-  user_choice == "Scissors" && computer_choice == "Paper" ||
-  user_choice == "Rock" && computer_choice == "Scissors"
+def tie
+  puts "We both played #{@computer_choice}! \nLet's play again or type \"stop\" to end the game."
 end
 
-def user_won(computer_choice)
+def user_wins?(user_choice)
+  user_choice == "Paper" && @computer_choice == "Rock" ||
+  user_choice == "Scissors" && @computer_choice == "Paper" ||
+  user_choice == "Rock" && @computer_choice == "Scissors"
+end
+
+def user_won
   @user_score += 1
-  puts "I played #{computer_choice}! YOU WIN!"
+  puts "I played #{@computer_choice}! YOU WIN!"
   puts_score
 end
 
-def computer_wins?(user_choice, computer_choice)
-  user_choice == "Rock" && computer_choice == "Paper" ||
-  user_choice == "Paper" && computer_choice == "Scissors" ||
-  user_choice == "Scissors" && computer_choice == "Rock"
+def computer_wins?(user_choice)
+  user_choice == "Rock" && @computer_choice == "Paper" ||
+  user_choice == "Paper" && @computer_choice == "Scissors" ||
+  user_choice == "Scissors" && @computer_choice == "Rock"
 end
 
-def computer_won(user_choice, computer_choice)
+def computer_won
   @computer_score += 1
-  puts "I played #{computer_choice}! I WIN!"
+  puts "I played #{@computer_choice}! I WIN!"
   puts_score
 end
 
@@ -44,15 +48,15 @@ def run_game
   puts "Let's play Rock, Paper, Scissors! \nGet ready with you choice!"
   user_choice = gets.chomp.capitalize
     while user_choice != "Stop"
-    computer_choice = ["Rock", "Paper", "Scissors"].sample
+      computer_selects
       if wrong_entry(user_choice)
         puts "I did not understand your choice. Try again or type \"stop\" to end the game."
-      elsif user_choice == computer_choice
-	      tie(user_choice)
-      elsif user_wins?(user_choice, computer_choice)
-        user_won(computer_choice)
-      elsif computer_wins?(user_choice, computer_choice)
-        computer_won(user_choice, computer_choice)
+      elsif user_choice == @computer_choice
+	      tie
+      elsif user_wins?(user_choice)
+        user_won
+      elsif computer_wins?(user_choice)
+        computer_won
       end
       user_choice = gets.chomp.capitalize
     end
